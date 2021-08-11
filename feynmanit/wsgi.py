62715eb64cc290385e0c80d08f1f2aec6804dfeb
Mario Molinito <unknownliteracy729videogames0@gmail.com>
2021-08-10 20:24:34 -0400

    Fixed permissions issue and User issue

---
+++ feynmanit/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for feynmanit project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feynmanit.settings')
+
+application = get_wsgi_application()
