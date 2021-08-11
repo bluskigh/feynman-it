04fc17eaf02a89ca2d081726ac3cceefe198cf03
molinitomario <unknownliteracy729videogames0@gmail.com>
2021-08-11 11:45:48 -0400

    Moved database to postgres

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
+from django.contrib.postgres.fields import ArrayField
 
 
 class User(AbstractUser):
@@ -7,4 +8,17 @@ class User(AbstractUser):
 
 
 class Note(models.Model):
-    pass
+    # title of the note
+    title = models.CharField(max_length=64)
+    # step one text(s)
+    step_one_iterations = ArrayField(default=list, base_field=models.TextField(blank=True))
+    # step two text(s)
+    step_two_iterations = ArrayField(default=list, base_field=models.TextField(blank=True))
+    # step three text
+    step_three = models.TextField(blank=True)
+    # links for step one
+    links = ArrayField(default=list, base_field=models.TextField(blank=True))
+    # does user understand new note
+    understand = models.BooleanField(default=False)
+    # who owns the note
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
