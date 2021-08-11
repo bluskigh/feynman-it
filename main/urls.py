9f5b9ba0f7bfe0d065015a2890528acdccb48e92
molinitomario <unknownliteracy729videogames0@gmail.com>
2021-08-11 12:11:45 -0400

    Revert "Revert "Moved database to postgres""
    This reverts commit 53e9b9ee2fb6cbef90a61d9cf99f8f8258b19dfa.

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
         path('', views.index, name='index'),
         path('profile/', views.profile, name='profile'),
+        path('notes/new', views.new_note, name='new_note'),
         path('accounts/login/', views.login, name='login'),
         path('accounts/register/', views.register, name='register'),
         path('accounts/logout', views.logout, name='logout')
